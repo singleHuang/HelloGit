f34af9581247d051a23d853a0a9f1b2ca9e0df94
Hjw <1342111235qq.com>
2021-05-11 22:35:59 +0800

    主干添加1

--- HelloGit/src/main/java/com/wuheng/git/HelloGit.java
+++ HelloGit/src/main/java/com/wuheng/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("更新1!");
         System.out.println("分支1!");
+        System.out.println("主干添加1!");
     }
 }
