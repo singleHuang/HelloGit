53b9f39a5a2061cccdaf0d1f542c1c99ce36e923
singleHuang <69023970+singleHuang@users.noreply.github.com>
2021-05-11 22:53:48 +0800

    Update HelloGit.java

--- HelloGit/src/main/java/com/wuheng/git/HelloGit.java
+++ HelloGit/src/main/java/com/wuheng/git/HelloGit.java
@@ -7,5 +7,6 @@ public class HelloGit {
         System.out.println("分支1!");
         System.out.println("主干添加1!");
         System.out.println("分支添加1!");
+        System.out.println("在线更新1!");
     }
 }
